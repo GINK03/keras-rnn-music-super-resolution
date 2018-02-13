bc9ad506e67b30487188c19f57b4ba7a47a2f664
gimpei kobayashi <gim.kobayashi@gmail.com>
2018-02-14 08:47:48 +0900

    諸々修正

--- 20-make-dataset.py
+++ 20-make-dataset.py
@@ -1,9 +1,9 @@
 import numpy as np
-
+from keras.callbacks       import LambdaCallback
 from scipy.io import wavfile 
 
 import pickle
-
+import time
 import random
 deg = np.array(wavfile.read('waves/degradation.wav')[1], dtype=np.int16)
 org = np.array(wavfile.read('waves/origin.wav')[1], dtype=np.int16)
@@ -25,32 +25,32 @@ for index, d in enumerate(range(len(deg)-1)):
   Ys.append(ys)
 
 sources, targets = [], []
-for i in range(0,len(Xs),20):
-  source = sum(Xs[i:i+20],[])
-  target = sum(Ys[i:i+20],[]) 
+for i in range(0,len(Xs),100):
+  source = sum(Xs[i:i+100],[])
+  target = sum(Ys[i:i+100],[]) 
   if random.random() > 0.8:
     continue
-  if len(source) == 100 and len(target) == 100:
+  if len(source) == 500 and len(target) == 500:
     sources.append( source )
     targets.append( target )
 
 length = len(sources)
-sources = np.array(sources, dtype=float).reshape((length, 100,1))/32767
-targets = np.array(targets, dtype=float).reshape((length, 100,1))/32767
+sources = np.array(sources, dtype=float).reshape((length, 500,1))/32767
+targets = np.array(targets, dtype=float).reshape((length, 500,1))/32767
 print(sources.shape)
 open('dataset.pkl', 'wb').write( pickle.dumps( (sources, targets) ) )
 
 
 sources, targets = [], []
-for i in range(0,len(Xs),20):
-  source = sum(Xs[i:i+20],[])
-  target = sum(Ys[i:i+20],[]) 
-  if len(source) == 100 and len(target) == 100:
+for i in range(0,len(Xs),100):
+  source = sum(Xs[i:i+100],[])
+  target = sum(Ys[i:i+100],[]) 
+  if len(source) == 500 and len(target) == 500:
     sources.append( source )
     targets.append( target )
 
 length = len(sources)
-sources = np.array(sources, dtype=float).reshape((length, 100,1))/32767
-targets = np.array(targets, dtype=float).reshape((length, 100,1))/32767
+sources = np.array(sources, dtype=float).reshape((length, 500,1))/32767
+targets = np.array(targets, dtype=float).reshape((length, 500,1))/32767
 print(sources.shape)
-open('predic.pkl', 'wb').write( pickle.dumps( (sources, targets) ) )
+open('predict.pkl', 'wb').write( pickle.dumps( (sources, targets) ) )
--- rnn-super-resolution.py
+++ rnn-super-resolution.py
@@ -11,6 +11,9 @@ from keras.layers.normalization import BatchNormalization as BN
 from keras.layers.noise         import GaussianNoise as GN
 from keras.layers.merge         import Concatenate
 from keras.layers.core          import Dropout
+from keras.layers.merge         import Concatenate as Concat
+from keras.layers.noise         import GaussianNoise as GN
+from keras.layers.merge         import Dot,Multiply
 import numpy as np
 import random
 import sys
@@ -19,44 +22,66 @@ import glob
 import copy
 import os
 import re
+import time
+input_tensor1 = Input(shape=(500, 1))
 
-input_tensor1 = Input(shape=(100, 1))
+x1           = Bi(GRU(300, dropout=0.1, recurrent_dropout=0.2, activation='linear', recurrent_activation='tanh', return_sequences=True))(input_tensor1)
 
-x           = Bi(GRU(500, dropout=0.1, recurrent_dropout=0.2, activation='relu', recurrent_activation='tanh', return_sequences=True))(input_tensor1)
-#x           = Bi(GRU(300, dropout=0.1, recurrent_dropout=0.2, activation='relu', recurrent_activation='tanh', return_sequences=True))(x)
+x2          = Flatten()(input_tensor1)
+x2          = Dense(1000, activation='relu')(x2)
+x2          = Dense(500, activation='relu')(x2)
+x2          = RepeatVector(500)(x2)
+x2          = Dense(500, activation='relu')(x2)
+x2          = Dense(600, activation='softmax')(x2)
+
+x3          = Multiply()([x1,x2])
+
+x           = Concatenate(axis=2)([x1,x3])
+
 x           = TD(Dense(5000, activation='relu'))(x)
-x           = BN()(x)
+x           = TD(Dense(5000, activation='relu'))(x)
+x           = Dropout(0.25)(x)
 x           = TD(Dense(3000, activation='relu'))(x)
-x           = BN()(x)
 x           = TD(Dense(3000, activation='relu'))(x)
-x           = BN()(x)
+x           = Dropout(0.25)(x)
 x           = TD(Dense(500, activation='relu'))(x)
-x           = Dropout(0.10)(x)
+x           = TD(Dense(500, activation='relu'))(x)
 decoded     = TD(Dense(1, activation='linear'))(x)
 
 model       = Model(input_tensor1, decoded)
 model.compile(optimizer=Adam(lr=0.0001, decay=0.03), loss='mae')
+
+buff = None
+now  = time.strftime("%H_%M_%S")
+def callback(epoch, logs):
+  global buff
+  buff = copy.copy(logs)
+batch_callback = LambdaCallback(on_epoch_end=lambda batch,logs: callback(batch,logs) )
+
 
 if '--train' in sys.argv:
   Xs, Ys = pickle.load(open('dataset.pkl', 'rb'))
   if '--resume' in sys.argv:
     model.load_weights(sorted(glob.glob('./models/000000049_0.000002000000.h5')).pop())
   print(Xs.shape)
-  decay = 0.02
-  init_rate =  0.00001
-  for i in range(50):
+  decay = 0.01
+  init_rate =  0.00005
+  for i in range(100):
     model.optimizer = Adam(lr=init_rate*(1.0 - decay*i))
     print("lr is {:.12f}".format(init_rate*(1.0 - decay*i)) )
-    model.fit(Xs,Ys, shuffle=True, epochs=1, batch_size=300)
-    model.save('models/{:09d}_{:.12f}.h5'.format(i,init_rate*(1.0 - decay*i)))
+    model.fit(Xs,Ys, shuffle=True, validation_split=0.1, epochs=1, batch_size=70, callbacks=[batch_callback])
+    loss = buff['loss']
+    val_loss = buff['val_loss']
+    model.save('models/{:.09f}_{:.09f}_{:09d}_{:.12f}.h5'.format(val_loss,loss,i,init_rate*(1.0 - decay*i)))
 
 import itertools
 from scipy.io import wavfile
 
 if '--predict' in sys.argv:
-  model.load_weights('models/000000019_00000.000002.h5')
+  model.load_weights('models/000000048_0.000004000000.h5')
   Xs, Ys = pickle.load(open('predict.pkl', 'rb'))
 
+  S = 1
   if '--baseline' in sys.argv: 
     Xs = Xs*32766
     xs = np.array(list(itertools.chain(*Xs.tolist())), dtype=np.int16)   
@@ -65,10 +90,10 @@ if '--predict' in sys.argv:
     Ys = Ys*32767
     ys = np.array(list(itertools.chain(*Ys.tolist())), dtype=np.int16)   
     wavfile.write(f'ys_orig_{0}.wav', 4410*4, ys)
-  
-  Yp = model.predict(Xs)
-  
-  Yp = Yp*32767
-  yps = np.array(list(itertools.chain(*Yp.tolist())), dtype=np.int16)   
-  wavfile.write(f'yp_orig_{0}.wav', 4410*4, yps)
+  else: 
+    Yp = model.predict(Xs)
+    
+    Yp = Yp*32767
+    yps = np.array(list(itertools.chain(*Yp.tolist())), dtype=np.int16)   
+    wavfile.write(f'yp_orig_{S}.wav', 4410*4, yps)
    
