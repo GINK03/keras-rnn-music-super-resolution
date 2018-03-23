a7290c0c3745b5dd8d2539d16824d1e5956000a3
gimpei kobayashi <gim.kobayashi@gmail.com>
2018-03-23 17:34:45 +0900

    量子化

--- 10-scan.py
+++ 10-scan.py
@@ -1,6 +1,6 @@
 from scipy.io import wavfile
 import numpy as np
-
+import pickle
 wav = wavfile.read('waves/reworu-wave.wav')
 #print(wav)
 # sample rate:44100
@@ -12,22 +12,28 @@ print('wave max', np.max(wav))
 
 # 16bit表現なので、0 ~ 65535に収まるはずであり、np.minが-32768なので足し合わせる
 nextwav = []
+Ys = []
 for index, w in enumerate(wav.tolist()):
   if w != 0 :
-    wabs = w + 32768
-    wabs = f'{wabs:016b}'
-    #print(index, w, wabs)
     nextwav.append(w)
+    Ys.append( w/32768 )
 nextwav = np.array(nextwav,dtype=np.int16 )
 print(nextwav.shape)
 wavfile.write('origin.wav', 44100, nextwav)
-sys.exit()
+#sys.exit()
 
 nextwav = []
+Xs = []
 for index, w in enumerate(wav.tolist()):
   if index%2 == 0:
-    #print(index, w)
+    wabs = w + 32768
+    wabs = f'{wabs:016b}'
+    Xs.append( wabs )
+    Xs.append( wabs )
     nextwav.append(w)
 nextwav = np.array(nextwav,dtype=np.int16 )
 print(nextwav.shape)
 wavfile.write('degradation.wav', 44100//2, nextwav)
+
+open('xs_ys.pkl', 'wb').write( pickle.dumps( (Xs, Ys) ) )
+
--- 20-make-dataset.py
+++ 20-make-dataset.py
@@ -4,52 +4,34 @@ from scipy.io import wavfile
 import pickle
 import time
 import random
-deg = np.array(wavfile.read('waves/degradation.wav')[1], dtype=np.int16)
-org = np.array(wavfile.read('waves/origin.wav')[1], dtype=np.int16)
 
-deg = deg.tolist()
-org = org.tolist()
+import sys
 
-Xs, Ys = [], []
-for index, d in enumerate(range(len(deg)-1)):
-  d = deg[d]
-  #print(d)
-  xs = [d]*5
-  ys = []
-  for i in range(5): 
-    key_org = index*5 + i
-    o = org[key_org]
-    ys.append(o)
-  Xs.append(xs)
-  Ys.append(ys)
+Xs, Ys = pickle.loads(open('xs_ys.pkl','rb').read())
 
-sources, targets = [], []
-for i in range(0,len(Xs),20):
-  source = sum(Xs[i:i+50],[])
-  target = sum(Ys[i:i+50],[]) 
-  if random.random() > 0.8:
+cross = list(zip(Xs, Ys))
+
+Xsa, Ysa = [], []
+for i in range(0, len(cross), 50):
+  _Xs, _Ys = [], []
+  for x, y in cross[i:i+50]:
+    #print( x, y )
+    nx = np.zeros(16)
+    for index, _x in enumerate(list(x)):
+      nx[index] = int(_x)
+    _Xs.append( nx )    
+    _Ys.append( y )
+  try:
+    _Xs = np.array(_Xs).reshape( (50, 16) ) 
+    _Ys = np.array(_Ys).reshape( (50, ) )
+  except Exception as ex:
+    print(ex)
     continue
-  if len(source) == 250 and len(target) == 250:
-    sources.append( source )
-    targets.append( target )
+  #print(_Xs.shape, _Ys.shape)
+  Xsa.append( _Xs )
+  Ysa.append( _Ys )
 
-length = len(sources)
-sources = np.array(sources, dtype=float).reshape((length, 250,1))/32767
-targets = np.array(targets, dtype=float).reshape((length, 250,1))/32767
-print(sources.shape)
-open('dataset.pkl', 'wb').write( pickle.dumps( (sources, targets) ) )
-
-
-sources, targets = [], []
-for i in range(0,len(Xs),50):
-  source = sum(Xs[i:i+50],[])
-  target = sum(Ys[i:i+50],[]) 
-  if len(source) == 250 and len(target) == 250:
-    sources.append( source )
-    targets.append( target )
-
-length = len(sources)
-sources = np.array(sources, dtype=float).reshape((length, 250,1))/32767
-targets = np.array(targets, dtype=float).reshape((length, 250,1))/32767
-print(sources.shape)
-open('predict.pkl', 'wb').write( pickle.dumps( (sources, targets) ) )
+Xsa, Ysa = map(np.array, [Xsa, Ysa])
+print(Xsa.shape, Ysa.shape)
+np.save('Xs', Xsa)
+np.save('Ys', Ysa)
